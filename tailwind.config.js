2facb75c3dd93caca021b6764734158a5a0cf3fa
orelbn <orel503@gmail.com>
2022-05-19 18:16:59 -0700

    fix: changed div to ul, changed index methodolgy to preserve component key in Array.map  between rerenders. feat: added opcaity animation on first load and new response generation.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,22 +2,19 @@ module.exports = {
   mode: "jit",
   content: ["./src/**/*.html", "./src/**/*.tsx"],
   theme: {
-    screens: {
-      xsm: "564px",
-      // => @media (min-width: 564px) { ... }
-      sm: "640px",
-      // => @media (min-width: 640px) { ... }
-
-      md: "768px",
-      // => @media (min-width: 768px) { ... }
-
-      lg: "1024px",
-      // => @media (min-width: 1024px) { ... }
-
-      xl: "1280px",
-      // => @media (min-width: 1280px) { ... }
-    },
     extend: {
+      screens: {
+        xsm: "564px",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        "fadeIn-response": "fadeIn 1s linear running",
+      },
       spacing: {
         128: "32rem",
       },
